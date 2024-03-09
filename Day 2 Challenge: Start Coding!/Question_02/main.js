7bdd7ad6e38de129b84f0424e2fa0a08a70cc86d
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-10 01:04:21 +0500

    Add files via upload

---
+++ Day 2 Challenge: Start Coding!/Question_02/main.js
@@ -0,0 +1,3 @@
+var famous_person = "Osama bin Adnan";
+var message = "".concat(famous_person, " once said, \"Eman hai to Imkan hai\"");
+console.log(message);
---
+++ Day 2 Challenge: Start Coding!/Question_02/main.ts
@@ -0,0 +1,3 @@
+let famous_person:string ="Osama bin Adnan";
+let message:string = `${famous_person} once said, "Eman hai to Imkan hai"`;
+console.log(message);
