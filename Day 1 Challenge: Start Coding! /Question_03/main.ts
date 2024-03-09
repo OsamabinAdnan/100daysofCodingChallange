d0544261177dcb628cb356926b3a83516a2fbd94
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-10 00:31:38 +0500

    Add files via upload

---
+++ Day 1 Challenge: Start Coding! /Question_03/main.ts
@@ -0,0 +1,7 @@
+let personName:string = "osama bin adnan";
+//Show name in small letter
+console.log(personName.toLowerCase());
+//Show name in BIG letter
+console.log(personName.toUpperCase());
+//Show name with the first letter Big/capital only
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
