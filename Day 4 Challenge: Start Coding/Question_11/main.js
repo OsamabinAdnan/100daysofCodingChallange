2c38f1f94e67af4563b83a1822b615637f178bae
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-11 01:55:39 +0500

    Add files via upload

---
+++ Day 4 Challenge: Start Coding/Question_11/main.js
@@ -0,0 +1,5 @@
+//Array function
+var names = ["Osama", "Talha", "Huzafa", "Hamdan", "Sufiyan", "Osman", "Arham"];
+for (var index = 0; index < names.length; index++) {
+    console.log(names[index]);
+}
