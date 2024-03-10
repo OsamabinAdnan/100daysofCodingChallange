9470b11094829c28c346cd9f1ee727c66d976393
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-11 01:56:42 +0500

    Add files via upload

---
+++ Day 4 Challenge: Start Coding/Question_12/main.js
@@ -0,0 +1,10 @@
+var names = ["Osama", "Talha", "Huzaifa", "Hamdan", "Sufiyan", "Osman", "Arham"];
+for (var index = 0; index < names.length; index++) {
+    var message = "'Would you like to learn some Typescript today?'";
+    console.log(names[index], message);
+}
+/*let names:string[] = ["Osama", "Talha","Huzaifa","Hamdan","Sufiyan","Osman", "Arham"];
+for (let name of names) {
+    console.log(`Hello ${name}, Would you like to learn some Typescript today`);
+}
+*/ 
