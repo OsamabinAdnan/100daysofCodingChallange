97623f4a166f47ae92161bb75cc0a2fa19732749
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-11 01:54:03 +0500

    Add files via upload

---
+++ Day 4 Challenge: Start Coding/Question_10/main.js
@@ -0,0 +1,3 @@
+// Osama bin Adnan 10th March 2024
+var myName = "Osama bin Adnan";
+console.log("".concat(myName, ", \"Would you like to learn some Typescript today?\""));
---
+++ Day 4 Challenge: Start Coding/Question_10/main.ts
@@ -0,0 +1,3 @@
+// Osama bin Adnan 10th March 2024
+let myName:String = "Osama bin Adnan";
+console.log(`${myName}, "Would you like to learn some Typescript today?"`);
