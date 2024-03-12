a8f22af01b9e5cc47e2d334d8ed4ad3365a52ce1
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-12 22:17:20 +0500

    Add files via upload

---
+++ Day 5 Challenge: Start Coding/Question_14/main.js
@@ -0,0 +1,4 @@
+var guestList = ["Talha", "Huzaifa", "Hamdan", "Sufiyan", "Osman", "Arham"];
+guestList.forEach(function (guestList) {
+    console.log("\"Hello ".concat(guestList, ", you are invited to dinner with me tommorow at 8 PM\""));
+});
