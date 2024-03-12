d649951d2e232e92d6a0b2b5ad55c07976c51361
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-12 22:16:27 +0500

    Add files via upload

---
+++ Day 5 Challenge: Start Coding/Question_13/main.js
@@ -0,0 +1,4 @@
+var transport = ["Honda Motorcycle", "Honda Civic Car", "Bus", "Chartered Plane"];
+transport.forEach(function (transport) {
+    console.log("\"I would like to own a ".concat(transport, ".\""));
+});
---
+++ Day 5 Challenge: Start Coding/Question_13/main.ts
@@ -0,0 +1,6 @@
+// Your own Array (Transporation modes)
+
+let transport:string[] = ["Honda Motorcycle", "Honda Civic Car", "Bus","Chartered Plane" ];
+transport.forEach(transport => {
+    console.log(`"I would like to own a ${transport}."`);
+});
