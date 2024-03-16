83bacb89651efe50a884f54a08d68e0deaa5f629
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-17 03:46:04 +0500

    Add files via upload

---
+++ Day 07 Challenge: Start Coding/Question_19/main.js
@@ -0,0 +1,5 @@
+"use strict";
+//Guest List
+let guestList = ["Talha", "Huzaifa", "Hamdan", "Sufiyan", "Osman", "Arham"];
+console.log(guestList.length);
+console.log(`"Hello, The party will be big and total ${guestList.length} peoples are coming for dinner"`);
---
+++ Day 07 Challenge: Start Coding/Question_19/main.ts
@@ -0,0 +1,6 @@
+//Guest List
+
+let guestList:string[] = ["Talha", "Huzaifa","Hamdan","Sufiyan","Osman","Arham"];
+console.log(guestList.length);
+console.log(`"Hello, The party will be big and total ${guestList.length} peoples are coming for dinner"`);
+
