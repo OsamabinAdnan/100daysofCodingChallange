3333512ca51bac2e8695719113b860f2d891b6b1
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-17 03:56:07 +0500

    Add files via upload

---
+++ Day 10 Challenge: Start Coding/Question_28/main.ts
@@ -0,0 +1,37 @@
+//Code 1 of stage of life
+let age:number = 35;
+if (age<18) {
+    console.log("Teenager")
+} 
+else if (age <=30 && age>=18){
+    console.log("Youth")
+}
+else if (age <=45 && age>30){
+    console.log("Middle Age")
+}
+else if (age <=60 && age>45){
+    console.log("Elder")
+}
+else if (age > 60){
+    console.log("Old")
+}
+// Code 2 of stages of life
+age = 25;
+if (age < 2) {
+    console.log("The person is baby.");
+}
+else if (age < 4) {
+    console.log("The person is toddler.");
+}
+else if (age < 13) {
+    console.log("The person is kid.");
+}
+else if (age < 20) {
+    console.log("The person is teenager.");
+}
+else if (age < 65) {
+    console.log("The person is adult.");
+}
+else {
+    console.log("The person is elder.");
+}
