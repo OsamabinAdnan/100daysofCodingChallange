eac98e1c164b3cdbc33cd2c0b3422b712de9743e
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-17 03:52:53 +0500

    Add files via upload

---
+++ Day 09 Challenge: Start Coding/Question_25/main.ts
@@ -0,0 +1,21 @@
+//if alien_color is green
+let alien_color = "green";
+if ( alien_color == "green") {
+    console.log ("Yes Color is Green, you just earned 10 points")
+} else {
+    console.log ("Color is not green")
+}
+//if alien_color is red
+alien_color = "red";
+if ( alien_color == "green") {
+    console.log ("Color is green")
+} else {
+    console.log ("Color is not green, it's Red")
+}
+//if alien_color is Yellow
+alien_color = "yellow";
+if ( alien_color == "green") {
+    console.log ("Color is green")
+} else {
+    console.log ("Color is not green, it's Yellow")
+}
