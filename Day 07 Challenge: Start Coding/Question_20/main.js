b65976a152ad004311beb702c1c3552d4b55a4ce
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-17 03:47:01 +0500

    Add files via upload

---
+++ Day 07 Challenge: Start Coding/Question_20/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Some countries names
+let countries = ['"Turkey", "Saudi Arabia", "Palestine", "Switzerland"'];
+console.log(`I would like to visit ${countries} `);
+//Some mountain names
+let mountains = [`"Evert", "K2, "Kangchenjunga","Rakaposhi"`];
+console.log(`Some mountains names are ${mountains} `);
