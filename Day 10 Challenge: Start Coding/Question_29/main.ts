29bdde94498a356f47a7b125887e9769f8e65f7f
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-17 03:56:40 +0500

    Add files via upload

---
+++ Day 10 Challenge: Start Coding/Question_29/main.ts
@@ -0,0 +1,21 @@
+let favoriteFruits = ["Mango", "Banana", "Peach", "Cherry","Guava"];
+if (favoriteFruits.includes("Mango")) {
+    console.log("Yes, Mango is in the list")
+} 
+if (favoriteFruits.includes("Banana")) {
+    console.log("Yes, Banana is in the list")
+}
+if (favoriteFruits.includes("Peach")) {
+    console.log("Yes, Peach is in the list")
+}
+if (favoriteFruits.includes("Cherry")) {
+    console.log("Yes, Cherry is in the list")
+}
+if (favoriteFruits.includes("Guava")) {
+    console.log("Yes, Guava is in the list")
+}
+if (favoriteFruits.includes("Apple")) {
+    console.log("No, Apple is not in the list")
+}
+
+
