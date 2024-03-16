22edbbbe8d5aa1903ed1f5248f3e6c2fde519a7d
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-17 03:49:22 +0500

    Add files via upload

---
+++ Day 08 Challenge: Start Coding/Question_22/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//find we produce array index error
+let countries = ["Pakistan", "Iran", "Iraq", "China"];
+console.log(countries[4]);
+//Now we are correcting error because arrays are zero-indexed and above index 4 is out of bound
+console.log(countries[0]);
+console.log(countries[1]);
+console.log(countries[2]);
+console.log(countries[3]);
---
+++ Day 08 Challenge: Start Coding/Question_22/main.ts
@@ -0,0 +1,8 @@
+//find we produce array index error
+let countries:string[] = ["Pakistan","Iran","Iraq","China"];
+console.log(countries[4]);
+//Now we are correcting error because arrays are zero-indexed and above index 4 is out of bound
+console.log(countries[0]);
+console.log(countries[1]);
+console.log(countries[2]);
+console.log(countries[3]);
