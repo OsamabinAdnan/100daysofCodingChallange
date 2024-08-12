c71ae8eac8190d3d9b1524b885e28fc6a70dc896
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-08-12 15:18:54 +0500

    Delete Day 07 Challenge: Start Coding directory

--- Day 07 Challenge: Start Coding/Question_20/main.ts
+++
@@ -1,6 +0,0 @@
-//Some countries names
-let countries:string[] = ['"Turkey", "Saudi Arabia", "Palestine", "Switzerland"'];
-console.log(`I would like to visit ${countries} `);
-//Some mountain names
-let mountains:string[] = [`"Evert", "K2, "Kangchenjunga","Rakaposhi"`];
-console.log(`Some mountains names are ${mountains} `);
