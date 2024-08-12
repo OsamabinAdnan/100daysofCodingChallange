5f4d0e9d52d0a96d01a8e48dd3f350e23ee7aae2
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-08-12 15:17:58 +0500

    Delete Day 02 Challenge: Start Coding! directory

--- Day 02 Challenge: Start Coding!/Question_02/main.js
+++
@@ -1,3 +0,0 @@
-var famous_person = "Osama bin Adnan";
-var message = "".concat(famous_person, " once said, \"Eman hai to Imkan hai\"");
-console.log(message);
--- Day 02 Challenge: Start Coding!/Question_03/main.ts
+++
@@ -1,5 +0,0 @@
-let Name:string = "\n\t Osama bin Adnan \t\n"
-console.log (Name);
-let withoutspaceName:string = Name.trim();
-console.log (withoutspaceName);
-
